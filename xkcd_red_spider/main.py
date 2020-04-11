e56ab8078abc8033a01ce87152928822fb290faa
I-Kang Ding <ikding@gmail.com>
2020-04-11 19:55:22 -0400

    First stab at the spider army (WIP)
    Boxes are too closely spaced

--- xkcd_red_spider/main.py
+++ xkcd_red_spider/main.py
@@ -9,25 +9,118 @@ from pyvista import examples
 import xkcd_red_spider.utils as utils
 
 
-def main():
+def main(color_spider="red", color_box="tan", color_buildings="white"):
     """[summary]
 
     [extended_summary]
     """
-    spider = utils.get_unit_cell_spider()
-    box = utils.get_unit_cell_box()
+    plotter = pv.Plotter()
 
-    rotated_spider, rotated_box = utils.scale_rotate_spider_box_unit_cell(
-        spider=spider, box=box, rotation=[("z", -90), ("y", 180)]
+    spider_army = {}
+
+    spider_army[(1, 0)] = utils.process_spider_box_unit_cell(
+        spider=utils.get_unit_cell_spider(),
+        box=utils.get_unit_cell_box(),
+        translation=[1, 0, 0]
+    )
+
+    spider_army[(0, 3)] = utils.process_spider_box_unit_cell(
+        spider=utils.get_unit_cell_spider(),
+        box=utils.get_unit_cell_box(),
+        rotation=[("z", -90), ("y", 180)],
+        translation=[0, 3, 0],
+    )
+
+    spider_army[(-1, -2)] = utils.process_spider_box_unit_cell(
+        spider=utils.get_unit_cell_spider(),
+        box=utils.get_unit_cell_box(),
+        rotation=[("z", 0), ("y", 180)],
+        translation=[-1, -2, 0]
+    )
+
+    spider_army[(3, -2)] = utils.process_spider_box_unit_cell(
+        spider=utils.get_unit_cell_spider(),
+        box=utils.get_unit_cell_box(),
+        rotation=[("z", 0), ("y", 180)],
+        translation=[3, -2, 0]
+    )
+
+    spider_army[(4, 0)] = utils.process_spider_box_unit_cell(
+        spider=utils.get_unit_cell_spider(),
+        box=utils.get_unit_cell_box(),
+        rotation=[("z", 180), ("y", -90)],
+        translation=[4, 0, 0]
+    )
+
+    spider_army[(6, -1)] = utils.process_spider_box_unit_cell(
+        spider=utils.get_unit_cell_spider(),
+        box=utils.get_unit_cell_box(),
+        rotation=[("z", -90)],
+        translation=[6, -1, 0]
+    )
+
+    spider_army[(8, 1)] = utils.process_spider_box_unit_cell(
+        spider=utils.get_unit_cell_spider(),
+        box=utils.get_unit_cell_box(),
+        translation=[8, 1, 0]
+    )
+
+    spider_army[(10, -1)] = utils.process_spider_box_unit_cell(
+        spider=utils.get_unit_cell_spider(),
+        box=utils.get_unit_cell_box(),
+        rotation=[("y", -90)],
+        translation=[10, -1, 0],
+    )
+
+    spider_army[(-2, 2)] = utils.process_spider_box_unit_cell(
+        spider=utils.get_unit_cell_spider(),
+        box=utils.get_unit_cell_box(),
+        rotation=[("z", -90)],
+        translation=[-2, -2, 0],
+    )
+
+    spider_army[(-4, 2)] = utils.process_spider_box_unit_cell(
+        spider=utils.get_unit_cell_spider(),
+        box=utils.get_unit_cell_box(),
+        rotation=[("y", 90)],
+        translation=[-4, 2, 0],
+    )
+
+    spider_army[(-6, -1)] = utils.process_spider_box_unit_cell(
+        spider=utils.get_unit_cell_spider(),
+        box=utils.get_unit_cell_box(),
+        rotation=[("y", 180)],
+        translation=[-6, -1, 0],
+    )
+
+    spider_army[(-7, 2)] = utils.process_spider_box_unit_cell(
+        spider=utils.get_unit_cell_spider(),
+        box=utils.get_unit_cell_box(),
+        rotation=[("x", -90)],
+        translation=[-7, 2, 0],
+    )
+
+    spider_army[(-7, -2)] = utils.process_spider_box_unit_cell(
+        spider=utils.get_unit_cell_spider(),
+        box=utils.get_unit_cell_box(),
+        rotation=[("y", 90)],
+        translation=[-7, -2, 0],
+    )
+
+    spider_army[(-9, -3)] = utils.process_spider_box_unit_cell(
+        spider=utils.get_unit_cell_spider(),
+        box=utils.get_unit_cell_box(),
+        translation=[-9, -3, 0],
     )
 
     buildings = utils.get_buildings()
     buildings.points *= 2
     buildings.translate([0, 0, -10])
-    plotter = pv.Plotter()
-    plotter.add_mesh(rotated_spider, 'r')
-    plotter.add_mesh(rotated_box, color="tan", show_edges=True)
-    plotter.add_mesh(buildings, color="white", show_edges=True)
+
+    for unit in spider_army.values():
+        plotter.add_mesh(unit[0], color=color_spider)
+        plotter.add_mesh(unit[1], color=color_box, show_edges=True)
+    plotter.add_mesh(buildings, color=color_buildings, show_edges=True)
     plotter.show()
 
 
