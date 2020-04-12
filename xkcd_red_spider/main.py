895586e9cf52c1c199f53b52a025d00be59ff673
I-Kang Ding <ikding@gmail.com>
2020-04-11 20:34:16 -0400

    Simplify how we call xkcd spider army
    Use get_xkcd_spider_army() & module level constant

--- xkcd_red_spider/main.py
+++ xkcd_red_spider/main.py
@@ -3,116 +3,74 @@
 To run::
     python xkcd_red_spider/main.py
 """
+from typing import Dict, List, Tuple
+
 import pyvista as pv
 from pyvista import examples
 
 import xkcd_red_spider.utils as utils
 
 
-def main(color_spider="red", color_box="tan", color_buildings="white"):
+XKCD_SPIDER_ARMY_COORD = {
+    (1, 0): None,
+    (0, 3): [("z", -90), ("y", 180)],
+    (-1, -2): [("z", 0), ("y", 180)],
+    (3, -2): [("z", 0), ("y", 180)],
+    (4, 0): [("z", 180), ("y", -90)],
+    (6, -1): [("z", 90)],
+    (8, 1): None,
+    (10, -1): [("y", -90)],
+    (-2, 2): [("z", -90)],
+    (-4, 2): [("y", 90)],
+    (-6, -1): [("y", 180)],
+    (-8, 2): [("x", -90)],
+    (-8, -2): [("x", -90)],
+    (-10, -3): None,
+}
+
+
+def get_xkcd_spider_army(
+    spider_army_coord: Dict[Tuple[int, int], List[Tuple[str, int]]] = XKCD_SPIDER_ARMY_COORD
+) -> Dict[str, Tuple[pv.PolyData, pv.PolyData]]:
     """[summary]
 
     [extended_summary]
+
+    Args:
+        spider_army_coord (Dict[Tuple[int, int], List[Tuple[str, int]]], optional): [description].
+        Defaults to XKCD_SPIDER_ARMY_COORD.
+
+    Returns:
+        Dict[str, Tuple[pv.PolyData, pv.PolyData]]: [description]
+    """
+    spider_army = {}
+    for spider_unit_coord, spider_unit_rotatation in spider_army_coord.items():
+        spider_army[spider_unit_coord] = utils.process_spider_box_unit_cell(
+            spider=utils.get_unit_cell_spider(),
+            box=utils.get_unit_cell_box(),
+            rotation=spider_unit_rotatation,
+            translation=list(spider_unit_coord) + [0],
+        )
+
+    return spider_army
+
+
+def main(color_spider="red", color_box="tan", color_buildings="white") -> pv.Plotter:
+    """[summary]
+
+    [extended_summary]
+
+    Args:
+        color_spider (str, optional): [description]. Defaults to "red".
+        color_box (str, optional): [description]. Defaults to "tan".
+        color_buildings (str, optional): [description]. Defaults to "white".
+
+    Returns:
+        pv.Plotter: [description]
     """
     plotter = pv.Plotter()
 
-    spider_army = {}
-
-    spider_army[(1, 0)] = utils.process_spider_box_unit_cell(
-        spider=utils.get_unit_cell_spider(),
-        box=utils.get_unit_cell_box(),
-        translation=[1, 0, 0]
-    )
-
-    spider_army[(0, 3)] = utils.process_spider_box_unit_cell(
-        spider=utils.get_unit_cell_spider(),
-        box=utils.get_unit_cell_box(),
-        rotation=[("z", -90), ("y", 180)],
-        translation=[0, 3, 0],
-    )
-
-    spider_army[(-1, -2)] = utils.process_spider_box_unit_cell(
-        spider=utils.get_unit_cell_spider(),
-        box=utils.get_unit_cell_box(),
-        rotation=[("z", 0), ("y", 180)],
-        translation=[-1, -2, 0]
-    )
-
-    spider_army[(3, -2)] = utils.process_spider_box_unit_cell(
-        spider=utils.get_unit_cell_spider(),
-        box=utils.get_unit_cell_box(),
-        rotation=[("z", 0), ("y", 180)],
-        translation=[3, -2, 0]
-    )
-
-    spider_army[(4, 0)] = utils.process_spider_box_unit_cell(
-        spider=utils.get_unit_cell_spider(),
-        box=utils.get_unit_cell_box(),
-        rotation=[("z", 180), ("y", -90)],
-        translation=[4, 0, 0]
-    )
-
-    spider_army[(6, -1)] = utils.process_spider_box_unit_cell(
-        spider=utils.get_unit_cell_spider(),
-        box=utils.get_unit_cell_box(),
-        rotation=[("z", 90)],
-        translation=[6, -1, 0]
-    )
-
-    spider_army[(8, 1)] = utils.process_spider_box_unit_cell(
-        spider=utils.get_unit_cell_spider(),
-        box=utils.get_unit_cell_box(),
-        translation=[8, 1, 0]
-    )
-
-    spider_army[(10, -1)] = utils.process_spider_box_unit_cell(
-        spider=utils.get_unit_cell_spider(),
-        box=utils.get_unit_cell_box(),
-        rotation=[("y", -90)],
-        translation=[10, -1, 0],
-    )
-
-    spider_army[(-2, 2)] = utils.process_spider_box_unit_cell(
-        spider=utils.get_unit_cell_spider(),
-        box=utils.get_unit_cell_box(),
-        rotation=[("z", -90)],
-        translation=[-2, 2, 0],
-    )
-
-    spider_army[(-4, 2)] = utils.process_spider_box_unit_cell(
-        spider=utils.get_unit_cell_spider(),
-        box=utils.get_unit_cell_box(),
-        rotation=[("y", 90)],
-        translation=[-4, 2, 0],
-    )
-
-    spider_army[(-6, -1)] = utils.process_spider_box_unit_cell(
-        spider=utils.get_unit_cell_spider(),
-        box=utils.get_unit_cell_box(),
-        rotation=[("y", 180)],
-        translation=[-6, -1, 0],
-    )
-
-    spider_army[(-8, 2)] = utils.process_spider_box_unit_cell(
-        spider=utils.get_unit_cell_spider(),
-        box=utils.get_unit_cell_box(),
-        rotation=[("x", -90)],
-        translation=[-8, 2, 0],
-    )
-
-    spider_army[(-8, -2)] = utils.process_spider_box_unit_cell(
-        spider=utils.get_unit_cell_spider(),
-        box=utils.get_unit_cell_box(),
-        rotation=[("y", 90)],
-        translation=[-8, -2, 0],
-    )
-
-    spider_army[(-10, -3)] = utils.process_spider_box_unit_cell(
-        spider=utils.get_unit_cell_spider(),
-        box=utils.get_unit_cell_box(),
-        translation=[-10, -3, 0],
-    )
-
+    spider_army = get_xkcd_spider_army()
     buildings = utils.get_buildings()
     buildings.points *= 1
     buildings.translate([0, 0, -7])
@@ -121,8 +79,10 @@ def main(color_spider="red", color_box="tan", color_buildings="white"):
         plotter.add_mesh(unit[0], color=color_spider)
         plotter.add_mesh(unit[1], color=color_box, show_edges=True)
     plotter.add_mesh(buildings, color=color_buildings, show_edges=True)
-    plotter.show()
+
+    return plotter
 
 
 if __name__ == "__main__":
-    main()
+    p = main()
+    p.show()
