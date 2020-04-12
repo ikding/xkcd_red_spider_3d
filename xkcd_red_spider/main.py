8f316beb980ddcb3058309b7d9168bbc75a7af34
I-Kang Ding <ikding@gmail.com>
2020-04-11 20:56:53 -0400

    Add extra spider on two boxes for xkcd fidelity

--- xkcd_red_spider/main.py
+++ xkcd_red_spider/main.py
@@ -24,14 +24,15 @@ XKCD_SPIDER_ARMY_COORD = {
     (-4, 2): [("y", 90)],
     (-6, -1): [("y", 180)],
     (-8, 2): [("x", -90)],
-    (-8, -2): [("x", -90)],
+    (-7, -2): [("y", 90)],
     (-10, -3): None,
 }
 
 
 def get_xkcd_spider_army(
-    spider_army_coord: Dict[Tuple[int, int], List[Tuple[str, int]]] = XKCD_SPIDER_ARMY_COORD
-) -> Dict[str, Tuple[pv.PolyData, pv.PolyData]]:
+    spider_army_coord: Dict[Tuple[int, int], List[Tuple[str, int]]] = XKCD_SPIDER_ARMY_COORD,
+    extra_spider: bool = True,
+) -> List[Tuple[pv.PolyData, pv.PolyData]]:
     """[summary]
 
     [extended_summary]
@@ -39,18 +40,38 @@ def get_xkcd_spider_army(
     Args:
         spider_army_coord (Dict[Tuple[int, int], List[Tuple[str, int]]], optional): [description].
         Defaults to XKCD_SPIDER_ARMY_COORD.
+        extra_spider (bool, optional): [description]. Defaults to True.
 
     Returns:
-        Dict[str, Tuple[pv.PolyData, pv.PolyData]]: [description]
+        List[Tuple[pv.PolyData, pv.PolyData]]: [description]
     """
-    spider_army = {}
+    spider_army = []
     for spider_unit_coord, spider_unit_rotatation in spider_army_coord.items():
-        spider_army[spider_unit_coord] = utils.process_spider_box_unit_cell(
-            spider=utils.get_unit_cell_spider(),
-            box=utils.get_unit_cell_box(),
-            rotation=spider_unit_rotatation,
-            translation=list(spider_unit_coord) + [0],
+        spider_army.append(
+            utils.process_spider_box_unit_cell(
+                spider=utils.get_unit_cell_spider(),
+                box=utils.get_unit_cell_box(),
+                rotation=spider_unit_rotatation,
+                translation=list(spider_unit_coord) + [0],
+            )
         )
+
+    # Add two extra spiders for fidelity with xkcd comic
+    if extra_spider and (spider_army_coord == XKCD_SPIDER_ARMY_COORD):
+        spider_army += [
+            utils.process_spider_box_unit_cell(
+                spider=utils.get_unit_cell_spider(),
+                box=utils.get_unit_cell_box(),
+                rotation=[("x", 90)],
+                translation=[-1, -2, 0],
+            ),
+            utils.process_spider_box_unit_cell(
+                spider=utils.get_unit_cell_spider(),
+                box=utils.get_unit_cell_box(),
+                rotation=[("z", 180)],
+                translation=[-4, 2, 0],
+            ),
+        ]
 
     return spider_army
 
@@ -73,11 +94,11 @@ def main(color_spider="red", color_box="tan", color_buildings="white") -> pv.Plo
     spider_army = get_xkcd_spider_army()
     buildings = utils.get_buildings()
     buildings.points *= 1
-    buildings.translate([0, 0, -7])
+    buildings.translate([0, 0, -10])
 
-    for unit in spider_army.values():
-        plotter.add_mesh(unit[0], color=color_spider)
-        plotter.add_mesh(unit[1], color=color_box, show_edges=True)
+    for unit in spider_army:
+        plotter.add_mesh(unit[0], color=color_spider)  # spider
+        plotter.add_mesh(unit[1], color=color_box, show_edges=True)  # box
     plotter.add_mesh(buildings, color=color_buildings, show_edges=True)
 
     return plotter
